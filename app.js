32d3964e0f4bd41883568019310c9fb70d7fae19
olexii <omedvedew@ukr.net>
2021-01-11 17:34:28 +0200

    changed api protocol

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let obj = {};
 console.log(obj);
 
-fetch('http://api.openweathermap.org/data/2.5/weather?q=lviv&units=metric&APPID=5d066958a60d315387d9492393935c19')
+fetch('https://api.openweathermap.org/data/2.5/weather?q=lviv&units=metric&APPID=5d066958a60d315387d9492393935c19')
 .then(data => data.json())
 .then(json => {
     console.log(json);
